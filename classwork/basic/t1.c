a265aba11d0080ea91315dff511ff3f33ac4ac7a
Pratiksahu-02 <pratiksahu1802@gmail.com>
2025-02-13 23:23:13 +0530

    new

---
+++ classwork/basic/t1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int arr[] = {10, 20, 30};
+    int *ptr = arr;
+
+    for (int i = 0; i < 3; i++) {
+        printf("Address: %p, Value: %d\n", (ptr + i), *(ptr + i));
+    }
+
+    return 0;
+}
